45f3c1e2efdef84b5a02024a88e5f30abdbdba05
libchaos <libchaos01@163.com>
2016-03-14 19:21:27 +0800

    载入配置文件

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # node-forum
 express node forum bootstrap
+
+- babel 设置
+**babel src -d target**
---
+++ src/server.js
@@ -0,0 +1,28 @@
+'use strict';
+/**
+* pratice node.js project
+* @author libchaos
+*/
+import path from 'path';
+import ProjectCore from 'project-core';
+
+const $ = global.$= new ProjectCore();
+//加载配置文件
+$.init.add((done) => {
+  $.config.load(path.resolve(__dirname, 'config.js'))
+  const env = process.env.NODE_ENV || null;
+  if(env){
+    $.config.load(path.resolve(__dirname, '../config', env+'.js'));
+  }
+  $.env = env;
+  done();
+});
+//初始化
+$.init((err) => {
+  if(err){
+    console.error(err);
+    process.exit(-1);
+  }else{
+    console.log('inited [env=%s]', $.env);
+  }
+});
